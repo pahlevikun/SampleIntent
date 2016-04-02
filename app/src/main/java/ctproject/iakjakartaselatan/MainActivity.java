f5d88d1cfc723c60a3d28f3f27d6ac1de65a2cb2
Farhan Yuda Pahlevi <farhan@Pahlevis-MacBook-Pro.local>
2016-04-02 23:54:44 +0700

    1.0

---
+++ app/src/main/java/ctproject/iakjakartaselatan/MainActivity.java
@@ -0,0 +1,15 @@
+package ctproject.iakjakartaselatan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //FILE JAVA DI PERTEMUAN KEDUA YA
+    }
+}
